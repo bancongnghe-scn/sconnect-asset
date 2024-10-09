1673d2af06e7557a69e26967b412125a9e6a7d9a
hoangnv <thienpt@s-connect.net>
2024-10-09 18:35:11 +0700

    build

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,7 +2,6 @@
 <aside id="sidebar" class="sidebar bg-sc" :class="activeSidebarSm ? 'tw-left-0' : 'menu'">
 
     <ul class="sidebar-nav" id="sidebar-nav">
-
         <li class="nav-item">
             <a class="nav-link bg-sc text-white" href="{{route('home')}}">
                 <i class="fa-solid fa-house text-white"></i>
@@ -11,39 +10,47 @@
         </li><!-- End Dashboard Nav -->
 
         <li class="nav-item">
-            <a class="nav-link bg-sc text-white" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#" >
+            <a class="nav-link bg-sc text-white" data-bs-target="#setting-nav" data-bs-toggle="collapse" href="#">
                 <i class="fa-solid fa-gear text-white" ></i>
                 <span>Cài đặt</span>
                 <i class="fa-solid fa-arrow-right tw-ml-2 text-white" ></i>
             </a>
-            <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <ul
+                id="setting-nav"
+                class="nav-content collapse
+                        {{ request()->is('asset-type-group*')
+                        || request()->is('asset-type*')
+                        || request()->is('industry*')
+                        || request()->is('supplier*')
+                        || request()->is('rbac*') ? 'show' : '' }}"
+                data-bs-parent="#sidebar-nav">
                 <li>
-                    <a href="{{route('asset.type-group.list')}}" class="text-white">
+                    <a href="/asset-type-group/list" class="text-white text-decoration-none">
                         <i class="fa-solid fa-whiskey-glass"></i><span>Nhóm tài sản</span>
                     </a>
                 </li>
                 <li>
-                    <a href="/asset-type/list" class="text-white">
+                    <a href="/asset-type/list" class="text-white text-decoration-none">
                         <i class="fa-solid fa-glass-water-droplet"></i><span>Loại tài sản</span>
                     </a>
                 </li>
                 <li>
-                    <a href="/industry/list" class="text-white">
+                    <a href="/industry/list" class="text-white text-decoration-none">
                         <i class="fa-solid fa-glass-water-droplet"></i><span>Ngành hàng</span>
                     </a>
                 </li>
                 <li>
-                    <a href="/supplier/list" class="text-white">
+                    <a href="/supplier/list" class="text-white text-decoration-none">
                         <i class="fa-solid fa-glass-water-droplet"></i><span>Nhà cung cấp</span>
                     </a>
                 </li>
                 <li>
-                    <a href="/rbac/role/list" class="text-white">
+                    <a href="/rbac/role/list" class="text-white text-decoration-none">
                         <i class="fa-solid fa-glass-water-droplet"></i><span>Vai trò</span>
                     </a>
                 </li>
                 <li>
-                    <a href="/rbac/permission/list" class="text-white">
+                    <a href="/rbac/permission/list" class="text-white text-decoration-none">
                         <i class="fa-solid fa-glass-water-droplet"></i><span>Quyền ứng dụng</span>
                     </a>
                 </li>
@@ -55,19 +62,19 @@
                 <span>Quản lý tài sản</span>
                 <i class="fa-solid fa-arrow-right tw-ml-2 text-white"></i>
             </a>
-            <ul id="asset-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <ul id="asset-nav" class="nav-content collapse {{ request()->is('contract*') || request()->is('contract-appendix*') || request()->is('shopping_plan*') ? 'show' : '' }}" data-bs-parent="#sidebar-nav">
                 <li>
-                    <a href="/contract/list" class="text-white">
+                    <a href="/contract/list" class="text-white text-decoration-none">
                         <i class="fa-solid fa-whiskey-glass"></i><span>Hợp đồng</span>
                     </a>
                 </li>
                 <li>
-                    <a href="/contract-appendix/list" class="text-white">
+                    <a href="/contract-appendix/list" class="text-white text-decoration-none">
                         <i class="fa-solid fa-whiskey-glass"></i><span>Phụ lục hợp đồng</span>
                     </a>
                 </li>
                 <li>
-                    <a href="/shopping_plan/list" class="text-white">
+                    <a href="/shopping_plan/list" class="text-white text-decoration-none">
                         <i class="fa-solid fa-whiskey-glass"></i><span>Kế hoạch mua sắm</span>
                     </a>
                 </li>
