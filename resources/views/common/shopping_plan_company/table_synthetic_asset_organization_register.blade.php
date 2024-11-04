9fe990ae0ffb9f05485db24ff2123c4d10278d9d
hoangnv <thienpt@s-connect.net>
2024-11-04 19:00:40 +0700

    build

--- resources/views/common/shopping_plan_company/table_synthetic_asset_organization_register.blade.php
+++ resources/views/common/shopping_plan_company/table_synthetic_asset_organization_register.blade.php
@@ -27,39 +27,45 @@
         </tr>
         </thead>
         <tbody>
-        <template x-for="(organization, index) in data.organizations" :key="index">
+        <template x-for="(organization,index) in data.organizations">
+            <template x-if="organization.total_price !== 0">
+                <template x-for="(assetRegister, stt) in organization.asset_register">
+                    <tr>
+                        <!-- Cột STT và tên tổ chức, chỉ xuất hiện cho hàng đầu tiên (stt === 0) -->
+                        <td x-text="index + 1" :rowspan="stt === 0 ? organization.asset_register.length : null" x-show="stt === 0"></td>
+                        <td x-text="organization.name" :rowspan="stt === 0 ? organization.asset_register.length : null" x-show="stt === 0" class="tw-font-bold"></td>
 
-            <!-- Khi total_price khác 0 -->
-            <template x-for="(assetRegister, stt) in organization.asset_register" :key="stt">
-                <tr x-show="organization.total_price !== 0">
-                    <td x-text="index + 1" :rowspan="stt === 0 ? organization.asset_register.length : null"
-                        x-show="stt === 0"></td>
-                    <td x-text="organization.name"
-                        :rowspan="stt === 0 ? organization.asset_register.length : null"
-                        x-show="stt === 0" class="tw-font-bold"></td>
-                    <td x-text="assetRegister.asset_type_name" class="text-center"></td>
-                    <template x-for="number in Array.from({ length: 12 }, (_, i) => i + 1)" :key="number">
-                        <td x-text="assetRegister.register[number - 1]" class="text-center">-</td>
+                        <!-- Cột loại tài sản -->
+                        <td x-text="assetRegister.asset_type_name" class="text-center"></td>
+
+                        <!-- Cột đăng ký -->
+                        <template x-for="number in Array.from({ length: 12 }, (_, i) => i + 1)">
+                            <td x-text="assetRegister.register[number - 1]" class="text-center">-</td>
+                        </template>
+
+                        <!-- Cột tổng đăng ký -->
+                        <td x-text="assetRegister.total_register" class="text-center"></td>
+
+                        <!-- Tổng giá của tổ chức, chỉ xuất hiện cho hàng đầu tiên (stt === 0) -->
+                        <td x-text="organization.total_price" :rowspan="stt === 0 ? organization.asset_register.length : null" x-show="stt === 0" class="text-center"></td>
+
+                        <!-- Cột cuối cùng, chỉ xuất hiện cho hàng đầu tiên (stt === 0) -->
+                        <td :rowspan="stt === 0 ? organization.asset_register.length : null" x-show="stt === 0" class="text-center">...</td>
+                    </tr>
+                </template>
+
+            </template>
+            <template x-if="organization.total_price === 0">
+                <tr>
+                    <td x-text="index+1" rowspan="1"></td>
+                    <td x-text="organization.name" rowspan="1" class="tw-font-bold"></td>
+                    <template x-for="number in Array.from({ length: 14 }, (_, i) => i + 1)">
+                        <td class="text-center">-</td>
                     </template>
-                    <td x-text="assetRegister.total_register" class="text-center"></td>
-                    <td x-text="organization.total_price"
-                        :rowspan="stt === 0 ? organization.asset_register.length : null"
-                        x-show="stt === 0" class="text-center"></td>
-                    <td :rowspan="stt === 0 ? organization.asset_register.length : null" x-show="stt === 0"
-                        class="text-center">...</td>
+                    <td x-text="organization.total_price" rowspan="1" class="text-center"></td>
+                    <td rowspan="1" class="text-center">...</td>
                 </tr>
             </template>
-
-            <!-- Khi total_price là 0 -->
-            <tr x-show="organization.total_price === 0">
-                <td x-text="index + 1" rowspan="1"></td>
-                <td x-text="organization.name" rowspan="1" class="tw-font-bold"></td>
-                <template x-for="number in Array.from({ length: 14 }, (_, i) => i + 1)" :key="number">
-                    <td class="text-center">-</td>
-                </template>
-                <td x-text="organization.total_price" rowspan="1" class="text-center"></td>
-                <td rowspan="1" class="text-center">...</td>
-            </tr>
         </template>
         </tbody>
     </table>
