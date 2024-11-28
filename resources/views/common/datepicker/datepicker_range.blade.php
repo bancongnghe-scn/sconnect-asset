835af7d5cfdbf91a2f3633d989178663f18c3ed6
hoangnv <thienpt@s-connect.net>
2024-11-28 10:36:20 +0700

    build

--- resources/views/common/datepicker/datepicker_range.blade.php
+++ resources/views/common/datepicker/datepicker_range.blade.php
@@ -2,6 +2,7 @@
     <input type="text" class="form-control datepicker"
            placeholder="{{$placeholder ?? 'Chọn ngày'}}" autocomplete="off"
            @if(isset($id)) id="{{$id}}" @endif
+           @if(isset($disabled)) :disabled="{{$disabled}}" @endif
            x-init="new AirDatepicker($el, {
                 range: true,
                 multipleDatesSeparator: ' - ',
@@ -9,11 +10,13 @@
                 clearButton: true,
                 locale: localeEn,
                 dateFormat: 'dd/MM/yyyy',
+                selectedDates: [
+                        new Date('{{ $start ?? null }}'),
+                        new Date('{{ $end ?? null }}')
+                    ],
                 onSelect: (selectedDates) => {
-                    $dispatch('date-change', {
-                        start: selectedDates.date[0] ? format(selectedDates.date[0], 'dd/MM/yyyy') : null,
-                        end: selectedDates.date[1] ? format(selectedDates.date[1], 'dd/MM/yyyy') : null
-                    });
+                    @if(isset($start)) {{$start}} = selectedDates.date[0] ? format(selectedDates.date[0], 'dd/MM/yyyy') : null @endif
+                    @if(isset($end)) {{$end}} = selectedDates.date[1] ? format(selectedDates.date[1], 'dd/MM/yyyy') : null @endif
                 }
             })"
     >
