f2be2ac2dc91f5fcec6ea6f56cbe5eeb11f681a3
hoangnv <thienpt@s-connect.net>
2024-11-30 11:43:33 +0700

    build

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -36,12 +36,11 @@ window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    wsHost: import.meta.env.VITE_PUSHER_HOST ?? `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws'],
+    key: 'asset_pusher_key',
+    wsHost: '10.7.0.8',
+    wsPort: 6002,
+    forceTLS: false,
+    enabledTransports: ['ws'], // Chỉ dùng ws (không dùng wss)
+    disableStats: true
 });
 
