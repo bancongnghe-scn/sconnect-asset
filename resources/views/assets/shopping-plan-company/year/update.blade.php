031955283c10d6e5e98c4d6b5b7de8c000582c13
hoangnv <thienpt@s-connect.net>
2024-12-04 12:09:10 +0700

    build

--- resources/views/assets/shopping-plan-company/year/update.blade.php
+++ resources/views/assets/shopping-plan-company/year/update.blade.php
@@ -76,14 +76,12 @@
 
                             <div>
                                 <label class="tw-font-bold">Thời gian đăng ký<span class="tw-ml-1 tw-text-red-600 mb-0">*</span></label>
-                                <template x-if="data.start_time !== null">
-                                    @include('common.datepicker.datepicker_range', [
+                                @include('common.datepicker.datepicker_range', [
                                        'placeholder' => 'Chọn thời gian đăng ký',
                                        'disabled' => '+data.status !== STATUS_SHOPPING_PLAN_COMPANY_NEW && +data.status !== STATUS_SHOPPING_PLAN_COMPANY_REGISTER',
                                        'start' => 'data.start_time',
                                        'end' => 'data.end_time',
-                                    ])
-                                </template>
+                                ])
                             </div>
 
                             <template x-if="listUser.length > 0">
@@ -131,7 +129,7 @@
                     <template x-if="+data.status !== STATUS_SHOPPING_PLAN_COMPANY_NEW">
                         <div class="mb-3">
                             <div class="active-link tw-w-fit">Thống kê</div>
-                            <div class="mt-3 overflow-x-auto custom-control tw-max-w-full">
+                            <div class="mt-3 overflow-x-auto custom-scroll tw-max-w-full">
                                 <table id="example2" class="table table-bordered dataTable dtr-inline"
                                        aria-describedby="example2_info">
                                     <thead>
