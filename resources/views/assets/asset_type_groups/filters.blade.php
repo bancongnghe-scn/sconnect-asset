b92594ae8561ed3dde04197d04f09d4a49e2579d
hoangnv <thienpt@s-connect.net>
2024-12-28 15:59:56 +0700

    buld

--- resources/views/assets/asset_type_groups/filters.blade.php
+++ resources/views/assets/asset_type_groups/filters.blade.php
@@ -1,19 +1,8 @@
-<div class="row">
-    <div class="col-12">
-        <div class="card">
-            <div class="card-body d-flex align-items-end form-group">
-                <div class="col-4">
-                    <label class="tw-font-bold">Tên nhóm tài sản</label>
-                    <input type="text" class="form-control" x-model="filters.name"
-                           placeholder="Nhập tên nhóm tài sản">
-                </div>
-                <div class="col-auto">
-                    <button @click="list(filters)" type="button" class="btn btn-sc">Tìm kiếm</button>
-                </div>
-                <div class="col-auto">
-                    <button @click="reloadPage()" type="button" class="btn btn-secondary">Xóa lọc</button>
-                </div>
-            </div>
-        </div>
+<div class="d-flex align-items-end form-group">
+    <div class="col-4">
+        <input type="text" class="form-control" x-model="filters.name" placeholder="Nhập tên nhóm tài sản" @keydown.enter="list(filters)">
+    </div>
+    <div class="col-auto">
+        <button @click="reloadPage()" type="button" class="btn btn-outline-danger">Xóa lọc</button>
     </div>
 </div>
