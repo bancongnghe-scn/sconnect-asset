91df04fb791e3f4bb0c90d860d15f6076d9d7733
hoangnv <thienpt@s-connect.net>
2024-12-31 10:09:58 +0700

    build

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('ping', function () {
+    return Maatwebsite\Excel\Facades\Excel::download(new App\Exports\OrderExport(), 'purchase_proposal.xlsx');
+
     return 'pong';
 });
 
