baf5d2a8960debbee4b5cc30074221131334cbd4
hoangnv <thienpt@s-connect.net>
2025-01-07 01:16:59 +0700

    build

--- app/Http/Middleware/CheckAuth.php
+++ app/Http/Middleware/CheckAuth.php
@@ -11,7 +11,7 @@ class CheckAuth
 {
     public function handle(Request $request, \Closure $next)
     {
-        Auth::loginUsingId(2);
+        Auth::loginUsingId(333);
         //                dd(Auth::user()->getAllPermissions()->pluck('name')->toArray());
 
         return $next($request);
