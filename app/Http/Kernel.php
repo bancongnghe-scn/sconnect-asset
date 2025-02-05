12233a59788040b820c3cc1478b6b7d7dd3031d0
hoangnv <thienpt@s-connect.net>
2025-02-05 12:02:35 +0700

    build

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,6 +46,11 @@ class Kernel extends HttpKernel
         //            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         //        ],
         'api' => [
+//            'throttle:api',
+//            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
