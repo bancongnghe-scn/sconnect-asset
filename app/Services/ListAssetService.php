717f36ac2bcd9fe3ef9cf218115107954d0f8e44
Truong NQ <truongnq@s-connect.net>
2025-02-13 15:34:35 +0700

    update fix bug asset

--- app/Services/ListAssetService.php
+++ app/Services/ListAssetService.php
@@ -564,7 +564,7 @@ class ListAssetService
             "description" => $request->assetEdit['description'],
             "seri_number" => $request->assetEdit['seri_number'],
             "location" => $request->location,
-            "date_purchase" => $request->assetEdit['date_purchase'],
+            "date_purchase" => Carbon::createFromFormat('d/m/Y', $request->assetEdit['date_purchase'])->format('Y-m-d'),
         ]);
     }
 }
