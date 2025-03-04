99594425b8a2e6fa78098b8d464fff34c2a4465b
hoangnv <thienpt@s-connect.net>
2025-03-04 10:36:29 +0700

    build

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -45,7 +45,6 @@ class CommentController extends Controller
 
             return response_success();
         } catch (\Throwable $exception) {
-            dd($exception);
             report($exception);
 
             return response_error();
