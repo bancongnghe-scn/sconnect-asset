d886f70d0cb3719a88288a16f884f1bd8f08ca2f
hoangnv <thienpt@s-connect.net>
2025-03-08 17:21:38 +0700

    build

--- app/Http/Controllers/Rbac/MenuController.php
+++ app/Http/Controllers/Rbac/MenuController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Services\MenuService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class MenuController extends Controller
 {
@@ -17,6 +18,7 @@ class MenuController extends Controller
     public function getMenuUserLogin()
     {
         try {
+            Log::info(7777777777);
             $result = $this->menuService->getMenuUser(Auth::id());
 
             return response_success($result);
