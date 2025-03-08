575f194690690c75c9bda3f3d74eafff344909f5
hoangnv <thienpt@s-connect.net>
2025-03-08 09:04:38 +0700

    build

--- app/Http/Resources/ListImportWarehouseAssetResource.php
+++ app/Http/Resources/ListImportWarehouseAssetResource.php
@@ -75,9 +75,8 @@ class ListImportWarehouseAssetResource extends JsonResource
 
         foreach ($data as &$value) {
             $priceLast           = (+$value['price']) + ((+$value['price'] / $totalPrice) * $totalCost);
-            $rounded             = ceil($priceLast / 1000) * 1000;
-            $formatted           = number_format($rounded, 3, '.', '');
-            $value['price_last'] = $formatted;
+            $rounded             = round($priceLast, 2);
+            $value['price_last'] = $rounded;
         }
 
         return $data;
