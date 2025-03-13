74dfbbd14899adeaeb32f222abc544228d7fda90
hoangnv <thienpt@s-connect.net>
2025-03-13 14:40:49 +0700

    build

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -35,15 +35,15 @@ import Pusher from 'pusher-js';
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: 'pusher',
+    broadcaster: "pusher",
     key: import.meta.env.VITE_PUSHER_APP_KEY,
-    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? 'mt1',
-    wsHost: import.meta.env.VITE_PUSHER_HOST ?? `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
-    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws','wss'],
-    wsPath: import.meta.env.PUSHER_APP_PATH ?? "/ws"
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? "mt1",
+    wsHost: import.meta.env.VITE_PUSHER_HOST, // Chỉ domain, không có cổng
+    wsPort: import.meta.env.VITE_PUSHER_SCHEME === "https" ? null : import.meta.env.VITE_PUSHER_PORT,
+    wssPort: import.meta.env.VITE_PUSHER_SCHEME === "https" ? null : import.meta.env.VITE_PUSHER_PORT,
+    forceTLS: import.meta.env.VITE_PUSHER_SCHEME === "https",
+    enabledTransports: ["ws", "wss"],
+    wsPath: import.meta.env.VITE_PUSHER_APP_PATH ?? "/ws",
 });
 
 
